bb6da138de2dde176f9436efedf763d1fea61fd8
soymi1k <soymilkneigh@gmail.com>
2021-04-08 17:13:09 +0900

    first android app end

--- app/src/main/java/com/example/lovetest/fragment/ResultFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/ResultFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.lovetest.R
+import kotlinx.android.synthetic.main.fragment_result.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,12 +33,51 @@ class ResultFragment : Fragment() {
         }
     }
 
+    var option = -1
+
+    lateinit var navController: NavController
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        option = arguments?.getInt("index")?:-1
+
         return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        setResult(option)
+
+        btn_home.setOnClickListener {
+            navController.navigate(R.id.action_resultFragment_to_mainFragment)
+        }
+    }
+    fun setResult(option:Int){
+        when(option){
+            1 -> {
+                tv_main.text = "You are a Quitter"
+                tv_sub.text = "You can let the person easily"
+
+            }
+            2 -> {
+                tv_main.text = "you should focus on yourself"
+                tv_sub.text = "you become really clingy to your ex"
+            }
+            3 -> {
+                tv_main.text = "you should take it easy"
+                tv_sub.text = "you can do crazy things no matter what it takes"
+            }
+            4 -> {
+                tv_main.text = " you are pretty mature"
+                tv_sub.text = "You can easily accept the break-up"
+            }
+        }
     }
 
     companion object {
@@ -57,4 +99,6 @@ class ResultFragment : Fragment() {
                 }
             }
     }
+
+
 }
